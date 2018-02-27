c376fcc0379602b769230c4d61a20c0746c4fb29
pawlenko <tiprosquad@gmail.com>
2018-02-27 22:31:40 +0100

    Article controller with views and all CRUD

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,69 @@
+class ArticlesController < ApplicationController
+    before_action :set_article, only: [:show, :edit, :update, :destroy]
+
+    def index
+        @articles = Article.all
+    end
+
+    def new
+        @article = Article.new
+        @counter = Article.count + 1
+        @categories =  Category.all
+        if !params[:id].blank? 
+            @article.category =  Category.find(params[:id])
+        end
+     end
+
+     def edit
+        @counter    =  Article.count 
+        @categories =  Category.all
+     end
+
+     def destroy
+        @article.destroy
+           respond_to do |format|
+           format.html { redirect_to ({:action=>"index"}), notice: 'Article was successfully destroyed.' }
+           format.json { head :no_content }
+        end
+     end
+
+
+     def update
+        respond_to do |format|
+            if @article.update(article_param)
+               format.html { redirect_to ({:action=>"index"}), notice: 'Article was successfully updated.' }
+               format.json { render :show, status: :ok, location:    @article}
+            else
+               format.html { render :edit }
+               format.json { render json:  @article.errors, status: :unprocessable_entity }
+            end
+         end
+     end
+
+     def create 
+        @article = Article.new(article_param)
+        @article.user = current_user
+        respond_to do |format|
+         if @article.save
+            format.html { redirect_to ({:action=>"index"}), notice: 'Article was successfully created.' }
+            format.json { render :index, status: :created, location: @article }
+         else
+            format.html { render :new }
+            format.json { render json: @article.errors, status: :unprocessable_entity }
+         end
+        end
+     end
+
+
+     private
+
+
+     def article_param
+        params.require(:article).permit(:name,:category_id,:visible,:content)
+     end
+
+     def set_article
+        @article = Article.find(params[:id])
+     end
+
+end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+    def errors_helper(object)
+        render(:partial=>'additional/errors',:locals=>{:object=>object})
+    end 
 end
