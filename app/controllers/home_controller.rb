82707ed485911feb3a20273afd6347d7a918c6de
pawlenko <tiprosquad@gmail.com>
2018-03-05 14:07:33 +0100

    Categories update

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,11 @@
 class HomeController < ApplicationController
 
   def index
-    @articles = Article.all
+      @articles = Article.all
+      @categories = Category.first(5)
+    if !params[:category_id].blank? 
+      @articles = Article.where("category_id in (?)", params[:category_id])
+    end
+   
   end
 end
